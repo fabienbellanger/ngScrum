a177fff33b67f46af3bd23d4217156e8dec1616d
Fabien Bellanger <fabien.bellanger@apitic.com>
2017-09-05 22:36:45 +0200

    Mise à jour des paquets Node Mise à jour de ngx-translate

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -44,7 +44,7 @@ export function HttpLoaderFactory(http: HttpClient)
         TranslateModule.forRoot({
             loader: {
                 provide:    TranslateLoader,
-                useFactory: HttpLoaderFactory,
+                useFactory: (HttpLoaderFactory),
                 deps:       [HttpClient],
             }
         }),
