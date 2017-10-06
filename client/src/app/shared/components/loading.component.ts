f58525b78c523e4ce9a8269f5f06f7e64813ccb3
Fabien Bellanger <fabien.bellanger@apitic.com>
2017-10-06 22:58:35 +0200

    Correction spinner suite à la mise à jour de Material

--- client/src/app/shared/components/loading.component.ts
+++ client/src/app/shared/components/loading.component.ts
@@ -5,17 +5,14 @@ import { Component } from '@angular/core';
     template: `
         <div class="loading">
             <div>
-                <mat-progress-spinner
-                    mode="indeterminate"
-                    value="100"
-                    style="height: 50px; width: 50px">
+                <mat-progress-spinner mode="indeterminate" value="100">
                 </mat-progress-spinner>
             </div>
             <h2 class="primary-color">{{ 'loading' | translate }}...</h2>
         </div>
     `,
     styles: [
-        '.loading {display: flex; flex-direction: row;  align-items: center; margin: 20px 0px 0px 20px}',
+        '.loading {display: flex; flex-direction: row;  align-items: center; margin: 20px 0px 0px 0px}',
         'h2 {margin: 20px}',
     ],
 })
