77b8cc74ea78db6a62861cac8912d2980bb13e75
Fabien Bellanger <fabien.bellanger@apitic.com>
2017-10-10 23:18:40 +0200

    Petit avancement

--- server/app/Repositories/StatisticsRepository.php
+++ server/app/Repositories/StatisticsRepository.php
@@ -16,26 +16,78 @@
          */
         public static function getCIR($year): ?array
         {
+            // 1. Récupération des durées par utilisateur et par tâche
+            // -------------------------------------------------------
             $query = '
                 SELECT
+                    users.id                       AS id,
                     users.lastname                 AS lastname,
                     users.firstname                AS firstname,
-                    application.name               AS applicationName,
                     task.id                        AS taskId,
-                    SUM(task_user.worked_duration) AS duration,
-                    COUNT(application.id)          AS nbApplications
+                    SUM(task_user.worked_duration) AS duration
                 FROM task_user
                     INNER JOIN task ON task.id=task_user.task_id
                     INNER JOIN users ON users.id=task_user.user_id
-                    INNER JOIN task_application ON task.id=task_application.task_id
-                    INNER JOIN application ON application.id=task_application.application_id
                 WHERE task_user.date LIKE :year
                 GROUP BY users.id, task.id;';
             $results = DB::select($query, ['year' => $year . '%']);
 
-            // TODO: Problème avec les tâches multi-applications
-            // Trop d'heures sont comptabilisées
-            dd($results);
+            $tabTasksDurationPerUser = [];
+            if ($results)
+            {
+                foreach ($results as $line)
+                {
+                    $id        = intval($line->id);
+                    $lastname  = $line->lastname;
+                    $firstname = $line->firstname;
+                    $taskId    = intval($line->taskId);
+                    $duration  = $line->duration;
+
+                    if (!isset($tabTasksDurationPerUser[$id]))
+                    {
+                        $tabTasksDurationPerUser[$id]['lastname']  = $lastname;
+                        $tabTasksDurationPerUser[$id]['firstname'] = $firstname;
+                        $tabTasksDurationPerUser[$id]['tasks']     = [];
+                    }
+
+                    $tabTasksDurationPerUser[$id]['tasks'][$taskId] = floatval($duration);
+                }
+            }
+
+            // 2. Récupération des applications
+            // --------------------------------
+            $query = '
+                SELECT
+                    task.id          AS taskId,
+                    application.id   AS applicationId,
+                    application.name AS applicationName
+                FROM task_user
+                    INNER JOIN task ON task.id=task_user.task_id
+                    LEFT JOIN task_application ON task_application.task_id=task.id
+                    LEFT JOIN application ON application.id=task_application.application_id
+                WHERE task_user.date LIKE :year
+                GROUP BY task.id, application.id;';
+            $results = DB::select($query, ['year' => $year . '%']);
+
+            $tabTasksApplications = [];
+            if ($results)
+            {
+                foreach ($results as $line)
+                {
+                    $taskId          = intval($line->taskId);
+                    $applicationId   = intval($line->applicationId);
+                    $applicationName = $line->applicationName;
+
+                    if (!isset($tabTasksApplications[$taskId]))
+                    {
+                        $tabTasksApplications[$taskId] = [];
+                    }
+
+                    $tabTasksApplications[$taskId][$applicationId] = $applicationName;
+                }
+            }
+
+            dd($tabTasksDurationPerUser, $tabTasksApplications);
         }
     }
     
